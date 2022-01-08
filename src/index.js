a41cb74d26d446cc60a747dd3bf1cb6f0aeca0cc
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-01-09 01:40:07 +0330

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import configureStore from './store/configureStore';
+import { bugAdded, bugResolved, bugRemoved, bugAssignedToUser, getUnresolvedBugs, getBugsbyUser } from './store/bugs';
+import { projectAdded } from './store/projects';
+import { userAdded } from './store/users';
+
+const store = configureStore();
+
+store.subscribe(() => {
+    console.log("Store Changed!", store.getState());
+});
+
+// store.dispatch(actions.bugAdded("Bug1"));
+// store.dispatch(actions.bugResolved(1));
+// store.dispatch(actions.bugRemoved(1));
+
+store.dispatch(userAdded({ name: "User1" }));
+store.dispatch(userAdded({ name: "User2" }));
+
+store.dispatch(bugAdded({ description: "Bug1" }));
+store.dispatch(bugAdded({ description: "Bug2" }));
+store.dispatch(bugResolved({ id: 1 }));
+store.dispatch(bugRemoved({ id: 1 }));
+store.dispatch(bugAssignedToUser({ bugId: 2, userId: 1 }));
+
+store.dispatch(projectAdded({ name: "project 1" }));
+
+const unresolvedBugs = getUnresolvedBugs(store.getState());
+const userBugs = getBugsbyUser(1)(store.getState());   //currying
+
+console.log(unresolvedBugs, userBugs);
