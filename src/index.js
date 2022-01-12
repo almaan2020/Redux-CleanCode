9a555888361ea382cb7c223a4e792120bd0a18d2
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2022-01-13 01:48:24 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import configureStore from './store/configureStore';
-import { bugAdded, bugResolved, bugRemoved, bugAssignedToUser, getUnresolvedBugs, getBugsbyUser } from './store/bugs';
+import { bugAdded, bugResolved, bugRemoved, bugAssignedToUser, getUnresolvedBugs, getBugsbyUser, addBug, loadBugs, resolveBug, assignBugToUser } from './store/bugs';
 import { projectAdded } from './store/projects';
 import { userAdded } from './store/users';
 
@@ -20,7 +20,7 @@ store.dispatch(bugAdded({ description: "Bug1" }));
 store.dispatch(bugAdded({ description: "Bug2" }));
 store.dispatch(bugResolved({ id: 1 }));
 store.dispatch(bugRemoved({ id: 1 }));
-store.dispatch(bugAssignedToUser({ bugId: 2, userId: 1 }));
+store.dispatch(bugAssignedToUser({ id: 2, userId: 1 }));
 
 store.dispatch(projectAdded({ name: "project 1" }));
 
@@ -32,7 +32,7 @@ console.log(unresolvedBugs, userBugs);
 store.dispatch((dispatch, getState) => {         //dispatching function
     //call an API
     //when the promise is resolved => dispatch()
-    dispatch({ type: "bugReceived", bugs: [1, 2, 3] });
+    dispatch({ type: "bugReceivedSample", bugs: [1, 2, 3] });
     console.log(getState());
     //when the promise is rejected => dispatch()
 }
@@ -41,3 +41,20 @@ store.dispatch((dispatch, getState) => {         //dispatching function
 store.dispatch({ type: 'error', payload: { message: "An error occurred" } });
 
 
+/**** API ****/
+/* get */
+// store.dispatch(
+//     actions.apiCallBegan({
+//         url: '/bugs',
+//         onSuccess: 'bugs/bugsRecieved'
+//     })
+// );
+store.dispatch(loadBugs());
+// setTimeout(() => store.dispatch(loadBugs()), 2000);
+
+/*post*/
+// store.dispatch(addBug({ description: "a" }));        //add bug from server(api)
+
+/*put*/
+// setTimeout(() => store.dispatch(resolveBug(1)), 2000);
+setTimeout(() => store.dispatch(assignBugToUser(4, 2)), 2000);
